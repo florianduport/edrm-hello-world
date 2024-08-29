167b260378d41bba5d374248183f6c7763cabdcb
Florian Duport <f.duport@programisto.fr>
2024-08-29 17:04:08 +0200

    module

--- hello-world/models/user.model.js
+++ hello-world/models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 
@@ -12,4 +12,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("UserHelloWorld", userSchema, 'users');
 
-module.exports = User;
+export default User;
